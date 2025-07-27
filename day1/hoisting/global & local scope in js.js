696c32b05b24c4b9ff2ed80e779d85f04cdd309b
Avi-123222 <dasavishek669@gmail.com>
2025-07-27 09:29:18 +0530

    global and local scope

---
+++ day1/hoisting/global & local scope in js.js
@@ -0,0 +1,14 @@
+let x = 10;
+
+
+const y = 20;
+var a = 120
+function f1(){
+    let b = 200
+    console.log("within the function x" , x)
+    console.log("within the function b", b)
+}
+f1()
+
+console.log("outside the function x" , x)
+    console.log("outside the function b", b)
