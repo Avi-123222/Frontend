4d7a85329722dea504018cabbf55649e380819a0
Avi-123222 <dasavishek669@gmail.com>
2025-07-27 07:34:55 +0530

    hoisting in js

---
+++ day1/hoisting/hositing in js.js
@@ -0,0 +1,11 @@
+console.log(username)
+var username = "Avishek"
+console.log(username)
+
+const lastname = "Das"
+console.log(lastname)
+
+sayHii()
+function sayHii(){
+    console.log("say hii")
+}
