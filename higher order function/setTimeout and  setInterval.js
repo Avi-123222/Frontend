2a4a32a49c25b8ce082ed53687dcf034393cbf0a
Avi-123222 <dasavishek669@gmail.com>
2025-07-29 07:46:42 +0530

    setTimeout and setInterval

---
+++ higher order function/setTimeout and  setInterval.js
@@ -0,0 +1,11 @@
+console.log(1)
+
+setTimeout(hello,5000)
+function hello(){
+    console.log("Hello everyone!!!!")
+}
+
+//console.log(3)
+//const id = setInterval(hello,2000)
+//console.log(id)
+console.log(setInterval(hello,2000))
