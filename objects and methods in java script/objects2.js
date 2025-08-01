d9a43f555d792f543a00f2859115e306d2cdb52c
Avi-123222 <dasavishek669@gmail.com>
2025-08-01 07:16:37 +0530

    commit msg here

--- objects and methods in java script/objects2.js
+++ objects and methods in java script/objects2.js
@@ -4,3 +4,18 @@ const person = {
 }
 //console.log(object)
 //person{}// error -> as person is declared with  const
+ console.log(person.firstname +" "+  person.lastname)
+
+
+ //let person2 ={
+   // firstName:"Avishek",
+   // lastName:"patra"
+ //}
+
+ //person2 = {}// empty object
+//const isdeleted = delete person.lastname
+//console.log(isdeleted)
+
+ Object.seal(person)//it restrict the Object,so that any addition and deletion of any key can't be possible(still updating of key and values are possible)
+
+ Object.freeze(person)//through freeze method,we can't make any kind of operation(except extracting values) inside the object
