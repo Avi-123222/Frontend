951a348483f3bf3433cf5c1b4aa80ddc3346ffc8
Avi-123222 <dasavishek669@gmail.com>
2025-08-07 08:24:00 +0530

    DOM in java script

---
+++ DOM/1. index.js
@@ -0,0 +1,36 @@
+document.children
+//HTMLCollection [html]
+//document.children(0)
+
+document.children[0]
+
+typeof document.children[0]
+'object'
+console.dir(document.children[0])
+
+undefined
+document.children[0].children
+
+document
+
+console.dir(document)
+
+
+
+document.body.children[0].innerHTML = 'Avishek'
+
+document.body.children[0].innerText = 'Das'
+
+document.body.children[0].innerText = '<b>Frontend Development</b>'//returns at it is
+
+document.body.children[0].innerHTML = '<b>Frontend Development</b>'// no it modifies
+
+document.body.children[0].innerText = '<b>Frontend Development</b>'
+
+document.body.children[0].innerText = '<b>Frontend Development</b>'
+
+document.body.children[0].innerHTML = '<b>Frontend Development</b>'
+
+document.body.children[2].innerHTML = 'Avishek das'
+
+
